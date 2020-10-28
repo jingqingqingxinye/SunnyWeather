30f55a6c3a43852ee25071cc4e9a1129fbbfd1b0
jingqingqing <jingqingqing@baidu.com>
2020-10-28 11:27:09 +0800

    添加卡片布局

---
+++ app/src/main/java/com/example/sunnyweather/CardViewActivity.java
@@ -0,0 +1,17 @@
+package com.example.sunnyweather;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author jingqingqing
+ * @date 2020/10/28
+ */
+public class CardViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardview);
+    }
+}
